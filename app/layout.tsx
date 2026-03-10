cc6679d94853775c2fceca6d259e42800f5abe1f
Manish Yadav <my491448@gmail.com>
2026-03-10 19:06:17 +0530

    fix: add type annotation for children prop in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import "./globals.css"
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
