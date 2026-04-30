d944e17a038c0a8a4a6565ad20c38862c3b924fa
manishh-89 <my491448@gmail.com>
2026-04-30 11:12:48 +0530

    Update favicon and add page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <title>G Digital India</title>
+        <link rel="icon" href="/images/logo.png" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </head>
       <body>
